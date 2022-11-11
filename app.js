f10a314f40879912e5ac37a4ac30920b916f368d
dawidlehai <haidawidle@gmail.com>
2022-11-11 14:36:15 +0100

    Created Express server and a simple GET response

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    tours,
+  });
+});
+
+app.listen(3000, () => console.log('Server started...'));
