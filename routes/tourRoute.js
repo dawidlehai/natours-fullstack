a52fc3620bae9e9dcb7650eea3a3f42ea458b8dd
dawidlehai <haidawidle@gmail.com>
2022-11-16 14:10:11 +0100

    Refactored createTour function for MongoDB

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const {
   getAllTours,
-  checkBody,
   createTour,
   getTour,
   updateTour,
@@ -11,7 +10,7 @@ const {
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
