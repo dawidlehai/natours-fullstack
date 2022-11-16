cca4c22e77bfad9c788308116ee7a068a2bfc7a7
dawidlehai <haidawidle@gmail.com>
2022-11-16 11:06:10 +0100

    Written app.js code

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   rules: {},
   globals: {
     __dirname: 'readonly',
+    process: 'readonly',
   },
 };
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoute');
+const userRouter = require('./routes/userRoute');
+
+const app = express();
+
+// Middlewares
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
