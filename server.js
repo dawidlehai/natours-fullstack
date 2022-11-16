38f279970a63f66a42b04d7cd7fc7e081f300137
dawidlehai <haidawidle@gmail.com>
2022-11-16 13:27:17 +0100

    Added basic schema

--- server.js
+++ server.js
@@ -8,11 +8,28 @@ const db = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
 
 mongoose
   .connect(db)
-  .then((connection) => {
-    console.log(connection.connections);
-    console.log('Connection established successfully.');
+  .then(() => {
+    console.log('Database connection established successfully.');
   })
   .catch((err) => console.log('Error:', err));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price.'],
+  },
+});
+
+const Tour = new mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
