3f987df57e244bf498c208156497217ef7a99936
dawidlehai <haidawidle@gmail.com>
2022-11-17 14:55:16 +0100

    Added different error responses for production and development modes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,8 +2,30 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
+  if (process.env.NODE_ENV === 'development') sendErrDev(err, res);
+  if (process.env.NODE_ENV === 'production') sendErrProd(err, res);
+};
+
+function sendErrDev(err, res) {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
+    stack: err.stack,
   });
-};
+}
+
+function sendErrProd(err, res) {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('⚠️ ERROR:', err);
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+}
