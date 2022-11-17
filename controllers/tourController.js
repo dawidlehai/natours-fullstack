80fee8e0c8957dec383240353cd19778cd2500be
dawidlehai <haidawidle@gmail.com>
2022-11-17 14:38:35 +0100

    Added error handling in tourController.js for null results by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
@@ -29,6 +30,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
 
+  if (!tour)
+    return next(new AppError(`No tour found with ID ${req.params.id}`, 404));
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -54,6 +58,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour)
+    return next(new AppError(`No tour found with ID ${req.params.id}`, 404));
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -63,7 +70,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour)
+    return next(new AppError(`No tour found with ID ${req.params.id}`, 404));
 
   res.status(204).json({
     status: 'success',
