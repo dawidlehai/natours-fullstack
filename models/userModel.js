33ae01c4294b07c03e1b8a1d0762329e8b833814
dawidlehai <haidawidle@gmail.com>
2022-11-20 10:20:03 +0100

    Added delete-me route and functionality

--- models/userModel.js
+++ models/userModel.js
@@ -40,6 +40,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 // DOCUMENT MIDDLEWARES
@@ -56,6 +61,14 @@ userSchema.pre('save', async function (next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+// QUERY MIDDLEWARES
+
+userSchema.pre(/^find/, function (next) {
+  // In query middlewares this points to the current query.
+  this.find({ active: { $ne: false } });
   next();
 });
 
