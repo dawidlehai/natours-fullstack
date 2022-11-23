7947995f4e339d18852cf88d2d8e05c88e079674
dawidlehai <haidawidle@gmail.com>
2022-11-23 10:39:28 +0100

    Added error site rendering

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,7 +4,7 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') sendErrDev(err, res);
+  if (process.env.NODE_ENV === 'development') sendErrDev(err, req, res);
   if (process.env.NODE_ENV === 'production') {
     let myErr = { ...err };
     myErr.message = err.message;
@@ -17,32 +17,54 @@ module.exports = (err, req, res, next) => {
     if (myErr.name === 'JsonWebTokenError') myErr = handleJwtError();
     if (myErr.name === 'TokenExpiredError') myErr = handleJwtExpiredError();
 
-    sendErrProd(myErr, res);
+    sendErrProd(myErr, req, res);
   }
 };
 
-function sendErrDev(err, res) {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+function sendErrDev(err, req, res) {
+  // API
+  if (req.originalUrl?.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  // Rendered website
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: err.message,
   });
 }
 
-function sendErrProd(err, res) {
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
+function sendErrProd(err, req, res) {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
     console.error('⚠️ ERROR:', err);
-    res.status(err.statusCode).json({
+    return res.status(err.statusCode).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
   }
+
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
+    });
+  }
+  console.error('⚠️ ERROR:', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later.',
+  });
 }
 
 function handleCastErrDB(err) {
