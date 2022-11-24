e749bdffe018abb6fb4563c74fe5097060666f94
dawidlehai <haidawidle@gmail.com>
2022-11-24 13:05:11 +0100

    Configured multer to save only images and specified unique filenames

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,31 @@
+const multer = require('multer');
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const filterBody = require('./../utils/filterBody');
 const factory = require('./handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, files, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) cb(null, true);
+  else cb(new AppError('Not an image! Please upload only images.', 400), false);
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
