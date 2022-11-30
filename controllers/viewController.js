246ad6e26cda6bc3508ddac1b22f8644281875c0
dawidlehai <haidawidle@gmail.com>
2022-11-30 15:09:59 +0100

    Added /my-tours route and rendering in user's account panel

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const Booking = require('../models/bookingModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
@@ -37,3 +38,17 @@ exports.getAccount = (req, res) => {
     user: req.user,
   });
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // 1) Find all bookings
+  const bookings = await Booking.find({ user: req.user.id });
+
+  // 2) Find tours with the returned IDs
+  const tourIDs = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
+  });
+});
