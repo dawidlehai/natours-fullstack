ab28eaf3c930ed2858755fbbee4f0ef0a1f323fb
dawidlehai <haidawidle@gmail.com>
2022-11-30 14:21:03 +0100

    Implemented temporary solution for creating booking at successful payment (not secure yet)

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -12,7 +12,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create Stripe checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -23,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/tour-2-cover.jpg`],
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
           },
           unit_amount: tour.price * 100,
         },
@@ -38,4 +40,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const viewController = require('./../controllers/viewController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
 // Commented out because in this case it is better to use it individually at each route rather than as a middleware
 // router.use(authController.isLoggedIn);
 
-router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
